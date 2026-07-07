96c1836e74b623b58f4a717e33e32be9607fe4f8
Joshua(Kayiio) <joginoparez@gmail.com>
2026-07-08 01:57:00 +0200

    [Add] I have add the Board game (board.py)
    + function afficher_plateau(plateau) who return a str (plat) with all
      the Cases of the board that i creat.
    
    Ps: Maybe add the case Shop for the future

--- board.py
+++ board.py
@@ -1,3 +1,14 @@
-plateau = ["normal", "bonus", "normal", "normal", "malus", "normal", "bonus", "normal", "normal", "malus"]
+board = ["normal", "bonus", "normal", "normal", "malus", "normal", "bonus", "normal", "normal", "malus"]
 
-print(f"voici le plateau de jeux : {plateau}")
+# print(f"voici le plateau de jeux : {plateau}") 
+
+def afficher_plateau(plateau):
+    n = len(plateau)
+    plat = ""
+
+    for i in range(n):
+        plat += f"[{plateau[i]}]"
+    
+    print(plat)
+
+afficher_plateau(board)
