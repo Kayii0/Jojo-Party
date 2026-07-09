4bdc0115e1393495e5ad739a616412ceffa77c5e
Joshua(Kayiio) <joginoparez@gmail.com>
2026-07-09 03:00:24 +0200

    [Add & Fix] Now we can have players on the board
    !/ I change the function afficher_plateau()
       - Now this function compare if a player is on the case and replace
         the case by his name and if two players are on the same case it
         will be displayed like [Alice/Bob]

--- board.py
+++ board.py
@@ -1,14 +1,31 @@
-board = ["normal", "bonus", "normal", "normal", "malus", "normal", "bonus", "normal", "normal", "malus"]
+from player import Joueur
 
+board = ["normal", "bonus", "normal", "normal", "malus", "normal", "bonus", "normal", "normal", "malus"]
+alice = Joueur("Alice")
+bob = Joueur("Bob")
+players = [alice, bob]
 # print(f"voici le plateau de jeux : {plateau}") 
 
-def afficher_plateau(plateau):
-    n = len(plateau)
+def afficher_plateau(plateau, joueurs):
+    nP = len(plateau)
+    nJ = len(joueurs)
     plat = ""
 
-    for i in range(n):
-        plat += f"[{plateau[i]}]"
-    
+    for i in range(nP):
+        joueurs_ici = []
+        contenu = f"[{plateau[i]}]"
+        for j in range(nJ):
+            if joueurs[j].position == i:
+                joueurs_ici.append(joueurs[j].nom)
+        
+        if joueurs_ici != []:
+            contenu = f"[{"/".join(joueurs_ici)}]"
+        else:
+            contenu = f"[{plateau[i]}]"
+        
+        plat += contenu
+            
+            
     print(plat)
 
-afficher_plateau(board)
+afficher_plateau(board, players)
--- player.py
+++ player.py
@@ -4,6 +4,3 @@ class Joueur:
         self.position = 0
         self.pieces = 5
 
-
-alice = Joueur("Alice")
-print(alice.nom, alice.position, alice.pieces)
